36457352c7d3ab1350f8a3e6b6cdd72f9274e5f7
Tharnid <jeffmsmithdev@gmail.com>
2021-10-20 00:00:44 -0400

    db setup successfully

--- server/server.js
+++ server/server.js
@@ -3,13 +3,36 @@ const {ApolloServer} = require('apollo-server-express');
 const http = require('http');
 require('dotenv').config();
 const path = require('path');
+const mongoose = require('mongoose');
 const { makeExecutableSchema } = require("graphql-tools");
 const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
 const { loadFilesSync } = require("@graphql-tools/load-files");
+const { truncate } = require('fs');
  
 
 // express server
 const app = express();
+
+// db
+
+const db = async () => {
+    try {
+        const success = await mongoose.connect(process.env.DATABASE_CLOUD, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+            // useFindAndModify: false
+        });
+        
+        console.log('DB Connected!!!');
+        
+    } catch (error) {
+        console.log('DB connection Error!!!', error);
+    }
+};
+
+// execute database connection
+db();
 
 // typeDefs
 
